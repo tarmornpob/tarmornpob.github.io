7bcb5ebfd6e6e015d361fc032a102d61b78600ac
tdev <tarmornpob@gmail.com>
2022-02-01 18:41:48 +0800

    Last summitted ❤ [01/02/22]

--- _next/static/6Nct4MTuWWuuya_MrwPAO/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/menu/contact":["static/chunks/pages/menu/contact-5237e0f4ab0ba317.js"],sortedPages:["/_app","/_error","/menu/contact"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/J-LNNGdMI6DtsPIgUgI-5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],"/port/phpjs-01":["static/chunks/pages/port/phpjs-01-a233e8333230bd5f.js"],sortedPages:["/_app","/_error","/port/phpjs-01"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
